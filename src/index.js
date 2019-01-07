bab11d967ff2995855cb130922d44fab04c507dd
Bruemor <arymer1@gmail.com>
2019-01-07 16:30:32 -0500

    Removed some files I no longer needed or wanted in here, updated components that were using them.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {MainHeading} from './components/index.js';
-import {ContentOneSection} from './components/section-2';
-import {Button} from './components/partials/button';
 import '../public/styles/index.scss';
 
 const Main = () => {
     return (
         <div className="container">
             <MainHeading />
-            <ContentOneSection />
-            <Button />
         </div>
     )
 }
