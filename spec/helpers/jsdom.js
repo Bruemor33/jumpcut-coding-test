e358540dfdfcc915e6e2e5666641714b4d3995e1
Bruemor <arymer1@gmail.com>
2019-01-07 14:54:12 -0500

    Brought babel-loader back, somehow it was uninstalled

--- spec/helpers/jsdom.js
+++ spec/helpers/jsdom.js
@@ -1,6 +1,6 @@
 import {JSDOM} from 'jsdom';
 
-const dom = new JSDOM('<html><body></body></html>');
+const dom = new JSDOM('<html><body><div id="app"></div></body></html>');
 global.document = dom.window.document;
 global.window = dom.window;
 global.navigator = dom.window.navigator;
