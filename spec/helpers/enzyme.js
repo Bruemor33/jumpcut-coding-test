26a1fe63eba4af82b5d91c1128cfd57120c2dc8e
Bruemor <arymer1@gmail.com>
2019-01-07 15:38:04 -0500

    Moved testing structure into new tests directory

--- spec/helpers/enzyme.js
+++
@@ -1,9 +0,0 @@
-import jasmineEnzyme from 'jasmine-enzyme';
-import {configure} from 'enzyme';
-import Adaptor from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adaptor() });
-
-beforeEach(() => {
-    jasmineEnzyme();
-});
--- spec/helpers/jsdom.js
+++
@@ -1,6 +0,0 @@
-import {JSDOM} from 'jsdom';
-
-const dom = new JSDOM('<html><body><div id="app"></div></body></html>');
-global.document = dom.window.document;
-global.window = dom.window;
-global.navigator = dom.window.navigator;
