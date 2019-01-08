370f99b30c7695837c6ff531526429a685e5a1e8
Bruemor <arymer1@gmail.com>
2019-01-07 23:04:38 -0500

    Created new component for housing the counter component as well as the value display

---
+++ src/components/counter-display.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Counter from './button-counter.js';
+import '../../public/styles/partials/count-display.scss';
+
+class CounterDisplay extends Component {
+
+    render() {
+        
+        return (
+            <div id="count-display" className="count-display-grid">
+                <h2>Count Value</h2>
+                <h4>{ this.props.count }</h4>
+                <div className="buttons-grid">
+                    <Counter />
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    }
+}
+
+export default connect(mapStateToProps)(CounterDisplay);
