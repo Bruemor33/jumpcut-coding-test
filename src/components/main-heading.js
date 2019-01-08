21e15313beaf0c0f3cda620cf8c20794df3f8800
Bruemor <arymer1@gmail.com>
2019-01-07 23:00:13 -0500

    Adjust which col class this component was using

--- src/components/main-heading.js
+++ src/components/main-heading.js
@@ -3,7 +3,7 @@ import '../../public/styles/partials/main-heading.scss';
 
 const MainHeading = () => {
     return (
-        <div id="main-heading" className="col-center-6">
+        <div id="main-heading" className="col-center-8">
             <h1>Click Me More</h1>
             <h4>The point of this application is to manipulate the state of a clickable button that tracks it's clicks</h4>
         </div>
