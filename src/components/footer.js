4bd00bd7d3f3328bf4d42620f9a6d7e94c681b99
Bruemor <arymer1@gmail.com>
2019-01-08 18:15:49 -0500

    Created footer component for plug

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+    render() {
+
+        return (
+            <div class="footer-grid">
+                <p>Project By: <a href="http://andrewrymer.io">Andrew Rymer</a></p>
+            </div>
+        )
+    }
+}
+
+export { Footer };
